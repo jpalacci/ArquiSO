48c8b7bd1ca8a63213062adc3c8146d606f13fe7
julian <jpalacci@itba.edu.ar>
2017-05-31 21:05:13 -0300

    Added libasm.asm

--- Kernel/asm/libasm.asm
+++ Kernel/asm/libasm.asm
@@ -1,4 +1,20 @@
 GLOBAL cpuVendor
+GLOBAL outputb
+GLOBAL inputb
+GLOBAL timerTickHandler
+EXTERN timerTickHandlerC
+GLOBAL sti
+GLOBAL keyboardHandler
+EXTERN keyboardHandlerC
+GLOBAL syscallHandler
+GLOBAL sys_write
+EXTERN syscall_writeC
+GLOBAL test
+GLOBAL mouse_handler
+EXTERN mouse_handlerC
+GLOBAL keyboardInitialize
+EXTERN keyboardInitializeC
+GLOBAL cli
 
 section .text
 	
@@ -25,3 +41,133 @@ cpuVendor:
 	mov rsp, rbp
 	pop rbp
 	ret
+
+inputb:
+	push rbp
+	mov rbp, rsp
+	sub rsp, 0x04
+	mov [rbp-0x04], edi
+	mov dx, word[rbp-0x04]
+	in al, dx
+	mov rsp,rbp
+	pop rbp
+	ret
+
+outputb:
+	push rbp
+	mov rbp, rsp
+	sub rsp, 0x08
+	mov[rbp-4], edi
+	mov [rbp-8], esi
+	mov dx, word[rbp-4]
+	mov al, byte[rbp-8]
+	out dx, al
+	mov rsp, rbp
+	pop rbp
+	ret
+
+sti:
+	sti
+	ret
+
+keyboardHandler:
+	cli
+	push rbp
+	mov rbp, rsp
+	call keyboardHandlerC
+	mov al, 20h
+	out 20h, al
+
+	mov rsp, rbp
+	pop rbp
+	sti
+	iretq
+
+timerTickHandler:
+		cli
+	push rbp
+	mov rbp, rsp
+
+	call timerTickHandlerC
+	mov al, 20h
+	out 20h, al	
+
+	mov rsp, rbp
+	pop rbp
+		sti
+	iretq
+
+syscallHandler:
+      
+        cmp rax,0x04
+        je sys_write
+        mov al, 20h
+        out 20h, al
+        mov rsp,rbp
+        pop rbp
+        iretq
+
+sys_write:
+    
+        push rbp
+        mov rbp,rsp
+        mov rdi,  rbx
+        mov rsi, rcx
+        call syscall_writeC
+        mov al, 20h
+        out 20h, al
+        mov rsp,rbp
+        pop rbp
+        iretq
+
+test:
+        mov al, 11101111b
+	out 0A1h,al
+	ret
+    
+mouse_handler:
+	cli
+	push rbp
+	mov rbp, rsp
+	call mouse_handlerC
+	mov al, 0x20
+		
+	
+	out 0xA0, al
+
+	out 0x20, al
+
+	mov rsp, rbp
+	pop rbp
+		sti
+	iretq
+
+cli:
+	cli
+	ret
+
+
+keyboardInitialize:
+	push rbp
+	mov rbp, rsp
+	mov rsi,0;
+	mov rdi, KeyBoardBuffer
+	mov rsi,0
+	mov rsi,100
+	call keyboardInitializeC
+	mov rsp, rbp
+	pop rbp
+	ret
+
+section .data
+
+msg db "hola mundo",0
+
+section .bss
+KeyBoardBuffer resb 100
+
+
+
+
+
+
